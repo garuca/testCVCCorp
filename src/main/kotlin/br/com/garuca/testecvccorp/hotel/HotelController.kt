b0284ef99dc22e23833f641b0d471ee056788b55
Gabriel <gabrilrc@gmail.com>
2019-01-29 09:38:01 -0200

    Limpando e separando respnsabilidades no controller e delegando au service.

--- src/main/kotlin/br/com/garuca/testecvccorp/hotel/HotelController.kt
+++ src/main/kotlin/br/com/garuca/testecvccorp/hotel/HotelController.kt
@@ -1,13 +1,15 @@
 package br.com.garuca.testecvccorp.hotel
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletRequest
-import org.springframework.web.client.RestTemplate
 
 
 @RestController
 class HotelController {
+    @Autowired
+    private val hotelService :HotelService? =null
     @GetMapping("/hotels")
     fun get(request: HttpServletRequest,@RequestParam("cityCode") cityCode:Int,
                                         @RequestParam("checkin") checkin:String,
@@ -15,15 +17,8 @@ class HotelController {
                                         @RequestParam("adult") adult:Int,
                                         @RequestParam("child") child:Int): Array<Hotel>?{
 
-       val restTemplate = RestTemplate()
-       val hotels :Array<Hotel>? = restTemplate.getForObject(
-               "https://cvcbackendhotel.herokuapp.com/hotels/avail/$cityCode",
-               Array<Hotel>::class.java)
-       hotels!!.forEachIndexed{ index, hotel->
-           hotel.calculate(checkin,checkout,adult,child)
-           hotels[index] =  hotel
-       }
 
-       return hotels
+        val hotels :Array<Hotel>? = hotelService?.getHotels(cityCode = cityCode)
+        return hotelService?.applyCommission(checkin,checkout,adult,child,hotels)
     }
 }
--- src/main/kotlin/br/com/garuca/testecvccorp/hotel/HotelService.kt
+++ src/main/kotlin/br/com/garuca/testecvccorp/hotel/HotelService.kt
@@ -1,8 +1,23 @@
 package br.com.garuca.testecvccorp.hotel
 
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
 
 @Service
 class HotelService {
+   fun getHotels(cityCode:Int?):Array<Hotel>?{
+       val restTemplate = RestTemplate()
+       return restTemplate.getForObject(
+               "https://cvcbackendhotel.herokuapp.com/hotels/avail/$cityCode",
+               Array<Hotel>::class.java)
 
+   }
+    fun applyCommission(checkin:String,checkout:String,adult:Int,child: Int,hotels:Array<Hotel>?):Array<Hotel>?{
+        hotels!!.forEachIndexed{ index, hotel->
+            hotel.calculate(checkin,checkout,adult,child)
+            hotels[index] =  hotel
+        }
+        return hotels
+
+    }
 }
